a98f932ea17ade7dba986d9dbad6b973e7e10ef1
Alexia <alexiaioana1225@gmail.com>
2026-01-16 11:21:49 +0200

    cifrul n

---
+++ cifrul +n/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cifrul_n
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void BtnCriptare_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(TxtN.Text);
+            TxtResult.Text = cifrul_n.Encrypt(TxtInput.Text, n);
+        }
+
+        private void BtnDecriptare_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(TxtN.Text);
+            TxtResult.Text = cifrul_n.Decrypt(TxtInput.Text, n);
+        }
+
+        private void BtnAnaliza_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResult.Text = cifrul_n.BruteForce(TxtInput.Text);
+        }
+    }
+}
---
+++ cifrul +n/cifrul n.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cifrul_n
+{
+    internal static class cifrul_n
+    {
+        
+        private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public static string Encrypt(string plain, int n)
+        {
+            if (string.IsNullOrWhiteSpace(plain)) return plain;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in plain)
+            {
+                if (char.IsLetter(c))
+                {
+                    char lowC = char.ToLower(c);
+                    int currentIndex = alphabet.IndexOf(lowC);
+                    int newIndex = (currentIndex + n) % 26;
+                    char processed = alphabet[newIndex];
+                    sb.Append(char.IsUpper(c) ? char.ToUpper(processed) : processed);
+                }
+                else sb.Append(c); 
+            }
+            return sb.ToString();
+        }
+
+        public static string Decrypt(string cipher, int n)
+        {
+            return Encrypt(cipher, (26 - n) % 26);
+        }
+
+        public static string BruteForce(string cipher)
+        {
+            StringBuilder results = new StringBuilder();
+            for (int n = 0; n < 26; n++)
+            {
+                string attempt = Decrypt(cipher, n);
+                results.AppendLine($"Cheia n={n}: {attempt}");
+            }
+            return results.ToString();
+        }
+    }
+}
